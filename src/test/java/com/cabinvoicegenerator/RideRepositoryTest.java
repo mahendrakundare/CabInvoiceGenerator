eb7285239a745b936a8eef6d928fd4fb76ea611d
mahendrakundare <mr.kundare@gmail.com>
2019-12-17 11:10:43 +0530

    Added Test Case to Check if no set of rides then retrun zero

--- src/test/java/com/cabinvoicegenerator/RideRepositoryTest.java
+++ src/test/java/com/cabinvoicegenerator/RideRepositoryTest.java
@@ -15,4 +15,13 @@ public class RideRepositoryTest {
         rideRepository.addRide(userId,ride);
         Assert.assertArrayEquals(ride,rideRepository.getRide(userId));
     }
+
+    @Test
+    public void givenUserNameWithNoRidesShouldThrowException() {
+        RideRepository rideRepository = new RideRepository();
+        String userId="123@gmail.com";
+        Ride[] rides = {};
+        rideRepository.addRide(userId,rides);
+        Assert.assertEquals(0,rideRepository.getRide(userId).length);
+    }
 }
